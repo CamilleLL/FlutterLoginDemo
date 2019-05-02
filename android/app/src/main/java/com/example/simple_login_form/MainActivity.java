e61864ef6b80f019d7fda432038ef15e64fcd5ed
Camille LE LUET <cleluet@infinitesquare.com>
2019-05-02 14:46:33 +0200

    Android splash

--- android/app/src/main/java/com/example/simple_login_form/MainActivity.java
+++ android/app/src/main/java/com/example/simple_login_form/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.simple_login_form;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -8,6 +10,16 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    GeneratedPluginRegistrant.registerWith(this); 
+
+    //Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
